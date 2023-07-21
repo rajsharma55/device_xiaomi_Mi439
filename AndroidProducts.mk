7a25228ad500ee4cc87621c20613266cf59c25f9
Raj Sharma <rajmwr5785@gmail.com>
2023-07-21 14:39:00 +0000

    Mi439: bringup arrowos
    Signed-off-by: Raj Sharma <rajmwr5785@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,13 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Mi439.mk \
-    $(LOCAL_DIR)/lineage_Mi439_4_19.mk
+    $(LOCAL_DIR)/arrow_Mi439.mk \
+    $(LOCAL_DIR)/arrow_Mi439_4_19.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Mi439-user \
-    lineage_Mi439-userdebug \
-    lineage_Mi439-eng \
-    lineage_Mi439_4_19-user \
-    lineage_Mi439_4_19-userdebug \
-    lineage_Mi439_4_19-eng
+    arrow_Mi439-user \
+    arrow_Mi439-userdebug \
+    arrow_Mi439-eng \
+    arrow_Mi439_4_19-user \
+    arrow_Mi439_4_19-userdebug \
+    arrow_Mi439_4_19-eng
