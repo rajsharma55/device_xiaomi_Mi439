450a70a5687adb206ae97fbaabc552f7a17b297e
Raj Sharma <rajmwr5785@gmail.com>
2023-07-21 14:44:11 +0000

    Mi439: set build fingerprint from google sunfish 23'july
    Signed-off-by: Raj Sharma <rajmwr5785@gmail.com>

--- arrow_Mi439.mk
+++ arrow_Mi439.mk
@@ -40,4 +40,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="olive-user 10 QKQ1.191014.001 V12.5.1.0.QCNMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/sunfish/sunfish:13/TQ3A.230705.001/10216780:user/release-keys"
